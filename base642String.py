c50b3378764aacdd6170f3d82bf8efb1fb2f1191
ominbruce <brucetheflame@outlook.com>
2020-10-15 13:20:39 +0800

    added base64 encoding and decoding tool

---
+++ base642String.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+A quick hacky tool created for picoCTF hexadecimal to string conversions
+"""
+
+__author__ = "Oaker Min"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import base64
+
+
+def base642Char():
+    fileWrite = "baseStringFlag.txt"
+    baseInput = input(
+        "Key in your base64 to convert. (E.g bDNhcm5fdGgzX3IwcDM1 = l3arn_th3_r0p35) : ").strip()
+
+    # Conversion from base64 to char
+    base64Bytes = baseInput.encode('ascii')
+    stringBytes = base64.b64decode(base64Bytes)
+    message = stringBytes.decode('ascii')
+
+    with open(fileWrite, "w") as fileWrite:
+        fileWrite.write("picoCTF{%s}" % (message) + "\n")
+    print("Your string converted from base64 is {}. Look inside your {}".format(
+        message, fileWrite))
+
+
+def main():
+    """ Main entry point of the app """
+    base642Char()
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
---
+++ string2Base64.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+A quick hacky tool created for picoCTF hexadecimal to string conversions
+"""
+
+__author__ = "Oaker Min"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import base64
+
+
+def char2Base64():
+    fileWrite = "stringBaseFlag.txt"
+    stringInput = input(
+        "Key in your base64 to convert. (E.g l3arn_th3_r0p35  = bDNhcm5fdGgzX3IwcDM1) : ").strip()
+
+    # Conversion from string to base64
+    stringBytes = stringInput.encode('ascii')
+    base64Bytes = base64.b64encode(stringBytes)
+    base64Message = base64Bytes.decode('ascii')
+
+    with open(fileWrite, "w") as fileWrite:
+        fileWrite.write("picoCTF{%s}" % (base64Message) + "\n")
+    print("Your base64 converted from strinng is is {}. Look inside your {}".format(
+        base64Message, fileWrite))
+
+
+def main():
+    """ Main entry point of the app """
+    char2Base64()
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
